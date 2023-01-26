8ed0ea5314ccd4443f022db6ed8b7f2f58167556
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-01-26 19:32:07 +1100

    Switching port to using remote archive.

--- ports/table-ws-protocol/portfile.cmake
+++ ports/table-ws-protocol/portfile.cmake
@@ -1,23 +1,18 @@
 # The CCI for the Proxy uploads an archive of the C++ protocol files.
 # The archive name includes the 7 character SHA1 of the commit that triggered the CI.
 # See: https://alphaengines.slack.com/archives/CNMEH4RSQ/p1674509868593579?thread_ts=1674074963.913839&cid=CNMEH4RSQ
-# set(COMMIT_SHA1 25b56fb)
-set(COMMIT_SHA1 fffffff)
 
-# vcpkg_extract_source_archive(
-#     SOURCE_PATH
-#     ARCHIVE "${SOURCE_ARCHIVE}"
-#     NO_REMOVE_ONE_LEVEL
-#     # (Optional) Read the docs for how to generate patches at:
-#     # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
-#     # PATCHES
-#     #   001_port_fixes.patch
-#     #   002_more_port_fixes.patch
-# )
+set(COMMIT_SHA1 1ce7064)
+
+vcpkg_download_distfile(SOURCE_ARCHIVE
+    URLS "https://download-site.prod.thinkalpha.io/table-ws-ng/table-ws-ng-headers-${COMMIT_SHA1}.tar.gz"
+    FILENAME "table-ws-protocol-local-1.0.0.tar.gz"
+    SHA512 1c0731fa122ad30405c883b25fd351a2de0b11bcab880a31107588a5fab0166c6c5f6b5372f059c17f735adfee62012ae7e28615d7e7cd51e14f1078cc417e62
+)
 
 vcpkg_extract_source_archive(
     SOURCE_PATH
-    ARCHIVE "\\\\wsl.localhost\\Debian\\home\\jarrod\\Dev\\table-ws-ng\\src\\protocol\\flatbuffer\\table-ws-ng-headers-${COMMIT_SHA1}.tar"
+    ARCHIVE "${SOURCE_ARCHIVE}"
     NO_REMOVE_ONE_LEVEL
     # (Optional) Read the docs for how to generate patches at:
     # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
@@ -29,10 +24,10 @@ vcpkg_extract_source_archive(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     # OPTIONS
-    #     -DRANGE_V3_TESTS=OFF
-    #     -DRANGE_V3_EXAMPLES=OFF
-    #     -DRANGE_V3_PERF=OFF
-    #     -DRANGE_V3_HEADER_CHECKS=OFF
+    #     -DTESTS=OFF
+    #     -DEXAMPLES=OFF
+    #     -DPERF=OFF
+    #     -DHEADER_CHECKS=OFF
 )
 
 vcpkg_cmake_install()
