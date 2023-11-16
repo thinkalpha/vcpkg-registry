66bacc0823fbf8a2fe83a145f644a63c70c30165
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-11-16 12:59:09 +1100

    Adding table-rdf portfile.

---
+++ ports/table-rdf/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO thinkalpha/table-rdf
+    REF 34d7d95a8d995d1294cc89e2e608c20a62cf48b2
+    SHA512 0
+    AUTHORIZATION_TOKEN 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
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    # OPTIONS
+    #     -DTESTS=OFF
+    #     -DEXAMPLES=OFF
+    #     -DPERF=OFF
+    #     -DHEADER_CHECKS=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/table-rdf)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+
+# Uncomment the line below if necessary to install the license file for the port
+# as a file named `copyright` to the directory `${CURRENT_PACKAGES_DIR}/share/${PORT}`
+# vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
