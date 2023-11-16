e8b0fae783e8e9eb5582ee1e846413b7a43dc307
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-11-17 09:44:33 +1100

    Updating table-rdf commit ref.

--- ports/table-rdf/portfile.cmake
+++ ports/table-rdf/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO thinkalpha/table-rdf
     HEAD_REF main
-    REF 4ec3ecbb58835171caf3378298a2fe6bc09f7ed5
-    SHA512 0
+    REF 00fc195a3fb1e969d15ef92bab131a249b183255
+    SHA512 e5240f9b3a3c7a15f721aebbeebf62a58a095dd77088c8f800dc96aad330b8af9467414b213ca203fef3c23ea19f21974ddfa6a453a2241e22f1e05038af5466
     HEAD_REF master
 )
 
