370e127fabe14d9bee3b2023a9e382d6f9f6aaae
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-11-17 10:56:03 +1100

    Updating git REF for table-rdf.

--- ports/table-rdf/portfile.cmake
+++ ports/table-rdf/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO thinkalpha/table-rdf
     HEAD_REF main
-    REF d916514ae180b63cf3e27a5d9eb32e7b75af2879
-    SHA512 d4c6a4c34613081744562aea259b4ea55c7fb8e4feb6ffe80c2f6db6fa8f217b2eb3eaebe8772e970783d1386a19448d7aea4a87c67cdaea75a9fef99d1394b4
+    REF ac10ac27988b148d5861368928aee272db215f9f
+    SHA512 4ddc4e8bced411f75ebc8dc6a51d0fde4ea58f105d2adacdf693f986adcad14de78e87a28b38e1c610a8d1b8c189b5892d15a1283c9db1e6ae86dcce8d10a60a
     HEAD_REF master
 )
 
