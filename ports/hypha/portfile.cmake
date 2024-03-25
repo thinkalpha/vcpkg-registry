d2e5ba8d96ca9b1e6f50134d850abdf238a44152
Jarrod Smith <jarrod.smith@ardentblue.com>
2024-03-25 23:37:59 +1100

    Updating hypha portfile, switching to using pre-built release artifact to make user's build process faster and simpler.

--- ports/hypha/portfile.cmake
+++ ports/hypha/portfile.cmake
@@ -1,9 +1,13 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO thinkalpha/hypha
-    HEAD_REF main
-    REF v0.5.0-alpha
-    SHA512 50acb467a2983ac4f99cd47f325ed1f51cfcbd4b33d95687f96e76d5425597b1c8bda47313ad51071b18dd7c7a856152839c6967fdd2adb51fc02e3a288f2f25
+vcpkg_download_distfile(
+    SOURCE_ARCHIVE
+    URLS "https://github.com/thinkalpha/hypha/releases/download/v0.5.0-alpha/hypha-0.5.0-Linux-x86_64-Release.tar.gz"
+    FILENAME "hypha-0.5.0-Linux-x86_64-Release.tar.gz"
+    SHA512 3977a45e1ba4ac94345a9c24089ff7f48064250b70a1f1fbadc38bfbe37b7617ee11810f74506532ab6ce603bb84423336830a1c3e2e5e7ab2feaf87f10bd3e1
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${SOURCE_ARCHIVE}"
 )
 
 vcpkg_cmake_configure(
@@ -14,4 +18,4 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/hypha)
 
-# file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
