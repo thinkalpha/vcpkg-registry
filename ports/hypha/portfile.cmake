811aa8a19d1166effaeaa71bcd24c8ff19e77143
Jarrod Smith <jarrod.smith@ardentblue.com>
2024-03-26 14:30:41 +1100

    Fix to passing wrong params to vcpkg_from_git().

--- ports/hypha/portfile.cmake
+++ ports/hypha/portfile.cmake
@@ -1,9 +1,8 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO thinkalpha/hypha
+    URL https://github.com/thinkalpha/hypha
     HEAD_REF main
     REF 30e5cc7e48c57687843c72c24531843feb5a03a9
-    SHA512 de7200c62cb9a34a932a9396d693e1eb45a1dd8e6efbfc4e0af94fa478f2fafd3f79c45bf2b2fe3bd12912f088ac45deec93b7f5c9e5af7a0d46df13075fa894
 )
 
 vcpkg_cmake_configure(
