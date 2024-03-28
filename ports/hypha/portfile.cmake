42df1f5fa5c6f946b620332c336f508cc87a070e
Jarrod Smith <jarrod.smith@ardentblue.com>
2024-03-28 12:59:36 +1100

    Switching link libs to PUBLIC in project CMakeLists.txt.

--- ports/hypha/portfile.cmake
+++ ports/hypha/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/thinkalpha/hypha
     HEAD_REF main
-    REF 30e5cc7e48c57687843c72c24531843feb5a03a9
+    REF 78f3cb92a0c2441f5072b82bdb822d5620e54c22
 )
 
 vcpkg_cmake_configure(
