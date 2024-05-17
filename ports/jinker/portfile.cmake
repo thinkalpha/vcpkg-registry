f0611a64229d6ec6d121ba26e826c599c484724a
Jarrod Smith <jarrod.smith@ardentblue.com>
2024-05-17 16:17:48 +1200

    Adding JINK_BUILD_TESTS=OFF to portfile.

--- ports/jinker/portfile.cmake
+++ ports/jinker/portfile.cmake
@@ -2,11 +2,13 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/thinkalpha/jinker
     HEAD_REF main
-    REF 8eda0e504e0ed74b30c9f98adbd19b4251be39b1
+    REF 47f16ad4aab62fd002681f1bc34fd0fc446f28d4
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DJINK_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
