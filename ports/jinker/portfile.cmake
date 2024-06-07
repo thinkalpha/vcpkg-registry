58921ebdca1b6ec51d9cf1ee6300472a44371cdc
Jarrod Smith <jarrod.smith@ardentblue.com>
2024-06-07 11:49:44 +1000

    Coupling JINK_BUILD_TESTS and Catch2 dependency to a vcpkg feature named "unit-test". Adding pkg-config support. Removing version specifiers for core dependencies. However Catch2 must still be >= 3.0.0.

--- ports/jinker/portfile.cmake
+++ ports/jinker/portfile.cmake
@@ -2,15 +2,21 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/thinkalpha/jinker
     HEAD_REF main
-    REF 47f16ad4aab62fd002681f1bc34fd0fc446f28d4
+    REF ce33911280189cca93a911db53f5f2e7cf6a72c0
+)
+
+# If the unit-tests feature is requested, then -DJINK_BUILD_TESTS=ON is added to FEATURE_OPTIONS.
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        unit-tests JINK_BUILD_TESTS
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DJINK_BUILD_TESTS=OFF
+        ${FEATURE_OPTIONS}      # e.g. -DJINK_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/jinker)
+vcpkg_fixup_pkgconfig()
